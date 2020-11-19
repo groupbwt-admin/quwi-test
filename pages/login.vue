52e3c372e78fad89de31cdc54591dce49ed843f5
taras.kuzma <taraskuzma87@gmail.com>
2020-11-19 17:57:08 +0200

    feat(pages): create login page

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h1>
+      Login page
+    </h1>
+    <form>
+      <div>
+        <input type="email">
+      </div>
+      <div>
+        <input type="password">
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style scoped>
+
+</style>
