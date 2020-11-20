3c640c5371630b1836f4881b556977d814dfc904
taras.kuzma <taraskuzma87@gmail.com>
2020-11-20 14:33:36 +0200

    feat(components): create navbar menu

---
+++ components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="nav nav-fixed">
+    <Logo/>
+    <ul class="nav-list">
+      <li class="nav-item">
+        <nuxt-link to="/" class="nav-link">
+          projects
+        </nuxt-link>
+      </li>
+      <li class="nav-item">
+        <a href="#"
+           class="nav-link"
+           @click.prevent="logout">
+          logout
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import Logo from "~/components/Logo"
+
+export default {
+  name: "Navbar",
+  components: {
+    Logo
+  },
+  methods: {
+    logout() {
+      this.$router.push("/")
+    }
+  }
+}
+</script>
+
