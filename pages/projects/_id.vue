e938e79b09cfd789d2be86c13e3282d7b4d9052c
taras.kuzma <taraskuzma87@gmail.com>
2020-11-20 18:30:33 +0200

    feat(pages): create project detailed page

---
+++ pages/projects/_id.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <section class="container-fluid back-block">
+      <div class="container">
+        <div class="row">
+          <a href="#" @click="back" class="back-link">back</a>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container project-details">
+        <div class="row">
+          <div class="title">name</div>
+          <div class="project-title" contenteditable="true">
+            quwi - desktop
+          </div>
+          <a href="#"
+             class="btn btn-blue"
+             @click.prevent="updateProjectName">
+            ok
+          </a>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    back() {
+      this.$router.back()
+    },
+    updateProjectName() {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.row {
+  align-items: center;
+  -webkit-align-items: center;
+  margin-bottom: 30px;
+}
+</style>
